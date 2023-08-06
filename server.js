c99b18b48da383aa5b5dbec0460661710849425d
Carlos Ferry <carlos.ferry@gmail.com>
2023-08-06 13:49:24 -0400

    Initialize server.

--- server.js
+++ server.js
@@ -3,11 +3,28 @@
 const { promises: Fs }  =   require('fs')
 const { Table }         =   require('console-table-printer');
 
+async function exists(file) 
+{
+    path = Path.join(__dirname, file)
+
+    try 
+    {
+         await Fs.access(path)
+         return true
+    } 
+    catch 
+    {
+         return false
+    }
+}
+
 async function initialize(check) 
 {
+    var server = http.createServer(app);
+
     await models.sequelize.sync().then(function () 
     {
-    
+                format.ok(`${'API server listening on'.padEnd(23)} ${server.address().address}:${server.address().port}`);
     });
 }
 
